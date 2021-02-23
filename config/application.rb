4a5bfa11822b0f417eaaf0ccafbeca90fef80493
KyosukeYanagihara <rctttzg7@gmail.com>
2021-02-23 11:32:16 +0900

    日本語設定、タイムゾーン設定

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Everyleaf
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.load_defaults 5.2
     config.generators do |g|
       g.test_framework :rspec,
