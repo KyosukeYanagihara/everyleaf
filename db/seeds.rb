1ba9aa8e0c4033c19ed33c8d939799ea4b1d1263
KyosukeYanagihara <rctttzg7@gmail.com>
2021-03-02 17:51:21 +0900

    seedデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(
-   email: 'test@test.com',
    name: 'テスト太郎',
+   email: 'test@test.com',
    password: '123456789',
    password_confirmation: '123456789',
    created_at: Time.current,
    updated_at: Time.current
+)
+User.create!(
+   name: '管理者',
+   email: 'admin@admin.com',
+   password: '123456789',
+   password_confirmation: '123456789',
+   admin: true,
+   created_at: Time.current,
+   updated_at: Time.current,
 )
 
 5.times do |n|
@@ -24,5 +33,18 @@ User.create!(
       created_at: Time.current,
       updated_at: Time.current,
       user_id: 1
+   )
+ end
+
+5.times do |n|
+   Task.create!(
+      name: "test#{n + 1}",
+      description: "test#{n + 1}",
+      deadline: Time.current.since(n.day),
+      status: "未着手",
+      priority: "低",
+      created_at: Time.current,
+      updated_at: Time.current,
+      user_id: 2
    )
  end
